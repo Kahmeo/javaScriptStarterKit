388d046ad7685100bf71d018014a7bfdc23763ca
Mehmet Ali Kahilogullari <kahilmehmetali@gmail.com>
2021-10-20 03:12:33 +0300

    18-ekim-ders-sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba kodlama.io")
+
+let dolorDun = 9.20
+
+const euroDun = 11.2
+// euroDun = 11 //Bu olmuyor
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi",12,"Özel Konut Projesi"]
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("<ul>")
